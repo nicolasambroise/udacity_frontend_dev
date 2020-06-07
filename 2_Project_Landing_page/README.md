fc31763abef05f7aa1d6a9823b392d2d6c6d1fde
Nicolas AMBROISE <darknico54@gmail.com>
2020-06-07 15:30:23 +0200

    Add Project 2 Landing Page

---
+++ 2_Project_Landing_page/README.md
@@ -0,0 +1,41 @@
+# Landing Page Project
+
+## Table of Contents
+
+* [Description](#description)
+* [Instructions](#instructions)
+* [Style-Guide](#style-guide)
+
+## Description
+
+In this project, you will be building a multi-section landing page. 
+Often times, you won’t know how much content will be added to a page through a Content Management System (CSM) or an API. 
+To handle this problem, you will dynamically add content to a web page. 
+You’ll be building a landing page that combines your skills with JavaScript, HTML, and CSS to update and control the page and create a dynamic user experience.
+
+#### Landing Page Behavior
+- Navigation is built dynamically as an unordered list.
+- It should be clear which section is being viewed while scrolling through the page.
+- When clicking an item from the navigation menu, the link should scroll to the appropriate section.
+
+## Instructions
+
+The starter project has some HTML and CSS styling to display a static version of the Landing Page project. 
+You'll need to convert this project from a static project to an interactive one. 
+This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+
+For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+#### Who to install it
+No installation, it's only html files.
+
+#### Who to use it
+Simply clic on index.html
+
+## Style-Guide
+
+See below for the Udacity Style Guide used thoroughout the Front End Nanodegree.
+
+* [Nanodegree Style Guide](http://udacity.github.io/frontend-nanodegree-styleguide/)
+
+http://udacity.github.io/frontend-nanodegree-styleguide/javascript.html
--- 2_Project_Landing_page/js/app.js
+++ 2_Project_Landing_page/js/app.js
@@ -18,6 +18,11 @@
  * 
 */
 
+const sections = document.getElementsByTagName("section");
+const navbar = document.getElementById("navbar__list");
+let current_section_id = ""; // current viewed section on screen (top of viewport is in section) 
+const section_offsetTop_margin = 40; // offset in px to ajust the top of viewport with the navbar element
+
 
 /**
  * End Global Variables
@@ -33,14 +38,83 @@
  * 
 */
 
-// build the nav
+// build the navbar
+function createNavbar() {
+    for (let i = 0; i < sections.length; i++) {
+        navbar.appendChild(createNavbarItem(sections[i]))
+    }
+}
 
+// build the navbar items
+function createNavbarItem(navigationItem) {
+    const div_elem = document.createElement("div");
+    const li_elem = document.createElement("li");
+	div_elem.classList.add("menu__link");
+    div_elem.appendChild(document.createTextNode(navigationItem.dataset.nav));
+	li_elem.appendChild(div_elem);
+	li_elem.setAttribute("data-nav", navigationItem.id);
+    return li_elem;
+}
 
 // Add class 'active' to section when near top of viewport
+function changeNavbarActive() {
+	const navbarItems = document.getElementsByClassName("menu__link");
+    const activeSection = getActiveSection();
+	
+	// change classes only if change section
+	if(activeSection.id != current_section_id){
+		
+		console.log("NEW activeSectionId:"+ activeSection.id);
+		
+		current_section_id = activeSection.id;
+		const activeNavbarItem = document.querySelector('#navbar__list li[data-nav="' + current_section_id + '"] .menu__link');		
+		
+		// remove navbar active class
+		for (let i = 0; i < navbarItems.length; i++) {
+			   navbarItems[i].classList.remove('menu__link__active');
+		}
+		// remove section active class
+		for (let i = 0; i < sections.length; i++) {
+			   sections[i].classList.remove('your-active-class');
+		}
+		// add navbar active class
+		activeNavbarItem.classList.add('menu__link__active');
+		
+		// add section active class
+		activeSection.classList.add('your-active-class');
+	}
+}
 
+function getActiveSection(){
+	let activeSection = sections[0];
+	let section_top_position = 0;
+	
+	// get current scroll position
+	const currentPosition = window.scrollY;
+	
+	for (let i = sections.length - 1; i >= 0; i--) {
+		// get selected section top position (offsetTop because of navbar and css padding)
+		section_top_position = sections[i].offsetTop - section_offsetTop_margin;
+		if(currentPosition >= sections[i].offsetTop - section_offsetTop_margin){
+			activeSection = sections[i];
+			break;
+		}
+	}
+	return activeSection;
+}
 
-// Scroll to anchor ID using scrollTO event
-
+// Scroll to anchor ID using scrollTO event (when navbar item click)
+function addSmoothScroll() {
+    const navbarItems = document.getElementsByClassName("menu__link");
+	let navbarTarget;
+    for (let i = 0; i < navbarItems.length; i++) {
+        navbarItems[i].parentElement.addEventListener("click", () => {
+			//console.log(navbarItems[i].parentElement);
+			navbarTarget = document.getElementById(navbarItems[i].parentElement.getAttribute("data-nav"));
+			navbarTarget.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"}); // smoth-scroll
+		});
+    }
+}
 
 /**
  * End Main Functions
@@ -49,9 +123,13 @@
 */
 
 // Build menu 
+createNavbar();
 
 // Scroll to section on link click
+addSmoothScroll();
 
 // Set sections as active
+changeNavbarActive();
+window.addEventListener('scroll',() => changeNavbarActive());
 
 
